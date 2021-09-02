f1f8546ac21ece8fc9d719ff59f2a360f3dbc5f6
Andrew Dewhirst <andrew.dewhirst8@gmail.com>
2021-09-01 22:30:35 -0400

    commit tree 95663a82a6364b74b46ec11890551e965556321d

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blockDistance) {
+    return Math.abs(blockDistance - 42)
+}
+
+function distanceFromHqInFeet(feetDistance) {
+    return (Math.abs(feetDistance - 42)*264)
+}
+
+function distanceTravelledInFeet(start,end) {
+    return (Math.abs(end - start)*264)
+}
+
+function calculatesFarePrice(beginning, destination) {
+    const distance = distanceTravelledInFeet(beginning, destination)
+    switch (true) {
+        case distance <400:
+            return 0;
+            break;
+        case (400 < distance && distance < 2000) :
+            return parseFloat(((distance*.02)-8).toFixed(2));
+            break;
+        case (2000 < distance && distance < 2500) :
+            return 25 ;
+            break;
+        case distance > 2500 :
+            return 'cannot travel that far';
+    }
+}
